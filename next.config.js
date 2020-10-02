64d59f46e9c4ceb6a24f8abca0d8910def139de5
frinzekt <frinzekt@gmail.com>
2020-10-03 01:48:08 +0800

    dotenv fix

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const Dotenv = require('dotenv-webpack')
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 const withSass = require('@zeit/next-sass');
@@ -7,6 +8,17 @@ const path = require('path');
 module.exports = withPlugins([[withSass], [optimizedImages, { handleImages: ['jpeg', 'png', 'svg', 'webp', 'ico'] }]], {
   webpack(config, options) {
     config.resolve.modules.push(path.resolve('./'));
+    config.plugins = config.plugins || []
+
+    config.plugins = [
+      ...config.plugins,
+
+      // Read the .env file
+      new Dotenv({
+        path: path.join(__dirname, '.env'),
+        systemvars: true
+      })
+    ]
     return config;
   },
 });
